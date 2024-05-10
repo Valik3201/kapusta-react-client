f04d223185dc3ad00f4c82f95c2df3c58d2f258a
Valik <valik3201@gmail.com>
2024-05-10 21:16:06 +0200

    feat(css): add responsive styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import kapustaLogo from "../../assets/logo.svg";
 const Header = () => {
   return (
     <header className="h-14 px-4 flex items-center bg-white">
-      <img src={kapustaLogo} className="logo" alt="Kapusta logo" />
+      <img src={kapustaLogo} alt="Kapusta logo" />
     </header>
   );
 };
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,17 +3,19 @@ const Logo = () => {
     <div>
       <span className="relative">
         <span
-          className="block absolute top-20 left-60 w-2 h-3.5 bg-black"
+          className="block absolute top-2 md:top-20 left-[7.5rem] md:left-60 w-1.5 md:w-2 h-2 md:h-3.5 bg-black"
           aria-hidden="true"
         ></span>
-        <h1 className="relative text-8xl font-extrabold">Kapusta</h1>
+        <h1 className="relative text-5xl md:text-8xl font-extrabold">
+          Kapusta
+        </h1>
         <span
-          className="block absolute top-4 left-60 w-2 h-3.5 bg-black"
+          className="block absolute top-10 md:top-4 left-[7.5rem] md:left-60 w-1.5 md:w-2 h-2 md:h-3.5 bg-black"
           aria-hidden="true"
         ></span>
       </span>
 
-      <p className="uppercase text-gray-darkest font-bold text-base tracking-[.18em] pl-16 pt-2">
+      <p className="uppercase text-gray-darkest font-bold text-sm md:text-base tracking-[.15em] md:tracking-[.18em] pl-4 md:pl-16 pt-2">
         Smart Finance
       </p>
     </div>
