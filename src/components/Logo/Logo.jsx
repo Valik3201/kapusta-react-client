1cbd70efad08e38c7798dcf0c705a5e326848318
Valik <valik3201@gmail.com>
2024-05-10 22:04:36 +0200

    refactor(css): update positioning from absolute to relative

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,12 +3,12 @@ const Logo = () => {
     <div>
       <span className="relative">
         <span
-          className="block absolute top-2 md:top-4 left-[7.5rem] md:left-60 w-1.5 md:w-2 h-2 md:h-3.5 bg-black"
+          className="block relative top-4 md:top-8 left-[7.5rem] md:left-60 w-1.5 md:w-2 h-2 md:h-3.5 bg-black"
           aria-hidden="true"
         ></span>
         <h1 className="text-5xl md:text-8xl font-extrabold">Kapusta</h1>
         <span
-          className="block absolute top-10 md:top-20 left-[7.5rem] md:left-60 w-1.5 md:w-2 h-2 md:h-3.5 bg-black"
+          className="block relative -top-2 md:-top-4 left-[7.5rem] md:left-60 w-1.5 md:w-2 h-2 md:h-3.5 bg-black"
           aria-hidden="true"
         ></span>
       </span>
