49ca2a99e6aeec463ec87fc7fa2010b13ddf4d41
Valik <valik3201@gmail.com>
2024-05-13 23:20:22 +0200

    feat(redux): update Redux

--- src/redux/auth/authSelectors.js
+++
@@ -1,6 +0,0 @@
-export const selectUser = (state) => state.auth.user;
-export const seelctIsLoading = (state) => state.auth.isLoading;
-export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
-export const selectAccessToken = (state) => state.auth.accessToken;
-export const selectIsRefreshing = (state) => state.auth.isRefreshing;
-export const selectError = (state) => state.auth.error;
