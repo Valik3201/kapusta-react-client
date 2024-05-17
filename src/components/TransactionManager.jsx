8e9a6e1ed4ec0cf69186bb24f9cdaa27ec563fb9
Valik <valik3201@gmail.com>
2024-05-17 15:34:59 +0200

    refactor: remove unnecessary import

--- src/components/TransactionManager.jsx
+++ src/components/TransactionManager.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectLoading, selectError } from "../redux/transactions/selectors";
 import TransactionForm from "./TransactionForm";
